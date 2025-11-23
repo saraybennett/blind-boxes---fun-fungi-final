ef0397079c3c5189e18d41b38a04b08b6a989b62
Your Name <your.email@example.com>
2025-11-23 21:51:44 +0300

    updates

--- public/app.js
+++ public/app.js
@@ -3,29 +3,23 @@ console.log("Hello!");
 //variables in global scope
 let imgOne = "./images/img1.png";
 let imgTwo = "./images/img2.png";
-let imgThree = "./images/img3.png";
-let imgFour = "./images/img4.png";
-let imgSecret = "./images/secret.png";
+// let imgThree = "./images/img3.png";
+// let imgFour = "./images/img4.png";
+// let imgSecret = "./images/secret.png";
 
 let secretProbability;
 
-//booleans to handle if a fungi has been unboxed or not
-let imgOneUnboxed = false;
-let imgTwoUnboxed = false;
-let imgThreeUnboxed = false;
-let imgFourUnboxed = false;
-let imgSecretUnboxed = false;
-
-// Array of fungi cohort
+// Array of fungi cohort - update this as appropriate!
 const fungiImages = [
   "./images/img1.png",
   "./images/img2.png",
-  "./images/img3.png",
-  "./images/img4.png",
-  "./images/secret.png",
+  // "./images/img3.png",
+  // "./images/img4.png",
+  // "./images/secret.png",
 ];
 
 const unboxedImages = [];
+let hasCollectedAll = false;
 
 //Initialize and connect socket
 let socket = io();
@@ -60,21 +54,14 @@ const showPopup = () => {
 
   //choose random image using chance library
   // probability = 0.01612903225; - can work on making this a slider that the users can control to highlight the way that this mechanism works and how hard it really is to get the secrets
-  secretProbability = 0.5;
-  let randomFungi = chance.weighted(fungiImages, [
-    1,
-    1,
-    1,
-    1,
-    secretProbability,
-  ]);
-  console.log(randomFungi);
+  secretProbability = 1;
+  let randomFungi = chance.weighted(fungiImages, [1, secretProbability]);
 
   const unboxedImage = document.querySelector(`img[src='${randomFungi}']`);
   unboxedImage.classList.add("unboxed");
 
   unboxedImages.push(randomFungi);
-  console.log("unboxed images:" + unboxedImages);
+  console.log(unboxedImages);
 
   //display results in popup window
   let fungiImagePick = document.createElement("img");
@@ -83,20 +70,24 @@ const showPopup = () => {
   fungiImagePick.height = 200;
   popup.appendChild(fungiImagePick);
 
-  //not doing anything currently, figure out if this is necessary
-  let resultDisplayOne = document.getElementById("popup_text");
+  //not doing anything currently, figure out if this is even necessary
+  // let resultDisplayOne = document.getElementById("popup_text");
 
   let continueButton = document.getElementById("continue_button");
   continueButton.addEventListener("click", function () {
     popup.style.visibility = "hidden";
     overlay.style.visibility = "hidden";
+
+    hasCollectedAll =
+      fungiImages.filter((x) => unboxedImages.includes(x)).length ==
+      fungiImages.length;
+
+    if (hasCollectedAll == true) {
+      //currently logging it for every item in the array, only want it happening once
+      // console.log("you've caught them all");
+      // alert("you've caught them all");
+      // location.reload();
+    }
+    console.log(hasCollectedAll);
   });
 };
-
-//function to track if something has been unboxed
-function fungiUnboxed() {
-  if ((randomFungi = imgOne)) {
-    console.log("imgOne picked");
-    imgOneUnboxed = true;
-  }
-}
